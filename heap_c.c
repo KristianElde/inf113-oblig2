1ea1333e2369e6bd484dc9eae8304e995d933d0d
Kristian Elde Johansen <kristian.elde.johansen@gmail.com>
2024-10-31 20:09:27 +0100

    .

--- heap_c.c
+++ heap_c.c
@@ -1,24 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include <stdlib.h>
-#include <stdio.h>
-
-void main() {
+void allocateOnce() {
     // Allocate 10 MB of memory
-    size_t size = 10 * 1024 * 1024;
+    // Pause to check memory without accessing it
+
+    size_t size = 100000 * 1024 * 1024;
     char *heap_array = (char *)malloc(size);
+    if (heap_array == NULL) {
+        printf("Too much memory allocated.");
+        return;
+    }
    
 
     printf("Allocated 10 MB at %p\n", heap_array);
-    getchar();  // Pause to check memory without accessing it
+    getchar(); 
+}
 
+void allocateMultipleTimes() {
+
+}
+
+void writingToMemory() {
     // Write to each page to bring it into physical memory
     for (size_t i = 0; i < size; i += 4096) {
         heap_array[i] = 1;  // Write to the start of each 4 KB page
     }
+}
 
-    printf("Memory accessed. Press Enter to continue...\n");
-    getchar();  // Pause again to inspect memory after access
+void main() {
 
 }
