f479b1d9f087d016c303d4486071e12df4c03a48
Kristian Elde Johansen <kristian.elde.johansen@gmail.com>
2024-10-31 18:49:04 +0100

    heap_c.c

---
+++ heap_c.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void main() {
+    // Allocate 10 MB of memory
+    size_t size = 10 * 1024 * 1024;
+    char *heap_array = (char *)malloc(size);
+   
+
+    printf("Allocated 10 MB at %p\n", heap_array);
+    getchar();  // Pause to check memory without accessing it
+
+    // Write to each page to bring it into physical memory
+    for (size_t i = 0; i < size; i += 4096) {
+        heap_array[i] = 1;  // Write to the start of each 4 KB page
+    }
+
+    printf("Memory accessed. Press Enter to continue...\n");
+    getchar();  // Pause again to inspect memory after access
+
+}
